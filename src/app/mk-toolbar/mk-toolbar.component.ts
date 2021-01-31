370877d8a2f66fe478fc7b187d0f2094400ea6ed
Madison Kriege <kriege.madison@gmail.com>
2021-01-30 19:28:59 -0600

    Angular Material

---
+++ src/app/mk-toolbar/mk-toolbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mk-toolbar',
+  templateUrl: './mk-toolbar.component.html',
+  styleUrls: ['./mk-toolbar.component.sass']
+})
+export class MkToolbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
